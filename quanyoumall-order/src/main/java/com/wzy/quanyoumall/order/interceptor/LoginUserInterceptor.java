898b5187e0982b0ffef17952cf85358c8d5f3b3d
JDI9527w <335100838@qq.com>
2025-03-16 23:46:07 +0800

    订单模块配置等.

---
+++ quanyoumall-order/src/main/java/com/wzy/quanyoumall/order/interceptor/LoginUserInterceptor.java
@@ -0,0 +1,27 @@
+package com.wzy.quanyoumall.order.interceptor;
+
+import com.wzy.quanyoumall.common.constant.AuthServerConstant;
+import com.wzy.quanyoumall.common.vo.MemberRespVo;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginUserInterceptor implements HandlerInterceptor {
+
+    public static ThreadLocal<MemberRespVo> loginUser = new ThreadLocal<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        MemberRespVo member = (MemberRespVo) request.getSession().getAttribute(AuthServerConstant.LOGIN_USER);
+        if (ObjectUtils.isNotEmpty(member)) {
+            loginUser.set(member);
+            return HandlerInterceptor.super.preHandle(request, response, handler);
+        } else {
+            request.getSession().setAttribute("msg", "请先登录");
+            response.sendRedirect("http://8.152.219.128/auth/login/login.html");
+            return false;
+        }
+    }
+}
